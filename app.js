fe37d4bc6b65cfa766008e8bcb9741907dec456f
Igor Juan <igor51juan@gmail.com>
2023-08-20 15:24:15 -0300

    Adicionando o while. Atividade 03.03

--- app.js
+++ app.js
@@ -1,18 +1,22 @@
 alert("Boas vindas ao jogo do número secreto");
 let numeroSecreto = 25;
 console.log(numeroSecreto);
-let chute = prompt("Escolha um número entre 1 e 30");
+let chute;
 
-// Adicione um console.log para verificar a comparação entre "chute" e "numeroSecreto"
+// Adcionando um console.log para verificar a comparação entre "chute" e "numeroSecreto"
 console.log('Resultado da comparação:', chute == numeroSecreto);
 
-// se o chute for igual ao número secreto
-if (chute == numeroSecreto) {
-    alert('Você acertou o número secreto que era: ' + numeroSecreto);
-} else {
-    if (chute > numeroSecreto) {
-        alert('O número secreto é menor que ' + chute);
+//Enquanto chute não for igual ao número secreto
+while (chute != numeroSecreto) {
+    chute = prompt("Escolha um número entre 1 e 30");
+    // se o chute for igual ao número secreto
+    if (chute == numeroSecreto) {
+        alert('Você acertou o número secreto que era: ' + numeroSecreto);
     } else {
-        alert('O número secreto é maior que ' + chute);
+        if (chute > numeroSecreto) {
+            alert('O número secreto é menor que ' + chute);
+        } else {
+            alert('O número secreto é maior que ' + chute);
+        }
     }
 }
