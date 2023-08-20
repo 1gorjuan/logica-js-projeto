c8bb98d2c20eeefc1bb644122d4580380f974024
Igor Juan <igor51juan@gmail.com>
2023-08-20 15:37:20 -0300

    Uso de tentativas aula 04.03

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ alert("Boas vindas ao jogo do número secreto");
 let numeroSecreto = 25;
 console.log(numeroSecreto);
 let chute;
+let tentativas = 1;
 
 // Adcionando um console.log para verificar a comparação entre "chute" e "numeroSecreto"
 console.log('Resultado da comparação:', chute == numeroSecreto);
@@ -11,12 +12,13 @@ while (chute != numeroSecreto) {
     chute = prompt("Escolha um número entre 1 e 30");
     // se o chute for igual ao número secreto
     if (chute == numeroSecreto) {
-        alert('Você acertou o número secreto que era: ' + numeroSecreto);
+        alert(`Parabéns! você acertou o número secreto, que era ${numeroSecreto} com ${tentativas} tentativas `);
     } else {
         if (chute > numeroSecreto) {
             alert('O número secreto é menor que ' + chute);
         } else {
             alert('O número secreto é maior que ' + chute);
         }
+        tentativas++;
     }
 }
