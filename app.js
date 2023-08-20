2288c6951da42dfea832c07b355141c997411567
Igor Juan <igor51juan@gmail.com>
2023-08-19 22:41:12 -0300

    Atualizando dados ensinado nas aulas

--- app.js
+++ app.js
@@ -1,3 +1,7 @@
 alert("Boas vindas ao jogo do número secreto");
 let SecretNumber = 29;
-prompt("Escolha um número entre 1 e 30")
+let UserGuess = prompt("Escolha um número entre 1 e 30");
+
+if (UserGuess == SecretNumber) {
+    console.log("Você acertou o número secreto");
+}
