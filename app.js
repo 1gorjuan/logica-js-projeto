86d89d7bf1173a315089e4b4bb9c300a0e089780
Igor Juan <igor51juan@gmail.com>
2023-08-20 13:47:26 -0300

    Atividade 03 Módulo 02

--- app.js
+++ app.js
@@ -1,7 +1,11 @@
 alert("Boas vindas ao jogo do número secreto");
-let SecretNumber = 29;
-let UserGuess = prompt("Escolha um número entre 1 e 30");
+let numeroSecreto = 25;
+console.log(numeroSecreto);
+let chute = prompt("Escolha um número entre 1 e 30");
 
-if (UserGuess == SecretNumber) {
-    console.log("Você acertou o número secreto");
+// se o chute for igual ao número secreto
+if (chute == numeroSecreto) {
+    alert("Você acertou o número secreto");
+} else {
+    alert('Você errou :(');
 }
